65c0f4152191ac989df3cc5856259d4e756da10b
zhehui.lim <zhehui.lim@alphatrade.com.au>
2026-05-27 16:28:23 +0800

    Prevent SendToTarget deadlock when peer is unresponsive

--- internal/session_settings.go
+++ internal/session_settings.go
@@ -23,6 +23,7 @@ type SessionSettings struct {
 	EnableResetSeqTime           bool
 	OutgoingMsgBufferSize        int
 	InChanCapacity               int
+	SocketWriteTimeout           time.Duration
 
 	// Required on logon for FIX.T.1 messages.
 	DefaultApplVerID string
